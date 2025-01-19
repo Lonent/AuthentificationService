2bd52020dcd49bbbbdfea30e9bf062b39e8fcfb2
Lonent <mlkvmhl@gmail.com>
2025-01-19 22:11:13 +0300

    Создано подключение через конфиг, исправлены ошибки

--- src/models/DatabaseModel.php
+++ src/models/DatabaseModel.php
@@ -1,37 +1,39 @@
 <?php
 namespace App\Models;
-class Database {
-    protected $host;
-    protected $dbname;
-    protected $username;
-    protected $password;
 
-    private $pdo;
-
-    function __construct($host, $dbname, $username, $password)
+class DatabaseModel {
+    function __construct()
     {
-        $this->host = $host;
-        $this->dbname = $dbname;
-        $this->username = $username;
-        $this->password = $password;
+        $config = require __DIR__ . '/../Config/dbconfig.php';
+        $this->host = $config['host'];
+        $this->dbname = $config['dbname'];
+        $this->username = $config['username'];
+        $this->password = $config['password'];
 
         $this->connect();
     }
 
     function connect(){
         try {
-            $this->pdo = new PDO(
+            $this->pdo = new \PDO(
                 "mysql:host={$this->host}; dbname={$this->dbname};",
                 $this->username,
                 $this->password);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
             return true;
-        } catch (PDOException $exception) {
+        } catch (\PDOException $exception) {
             return false;
         }
     }
 
+    /**
+     * Вставляет новую запись в таблицу.
+     *
+     * @param string $table Название таблицы.
+     * @param array $data Ассоциативный массив данных для вставки (ключи — это названия колонок) Например 'login' => 'TestLogin'.
+     * @return int|bool Возвращает ID последней вставленной записи или false в случае ошибки.
+     */
     function create($table, $data)
     {
         $columns = implode(', ', array_keys($data));
@@ -45,14 +47,24 @@ class Database {
             return false;
         }
     }
-    function read($table, $conditions=[], $columns)
+
+    /**
+     * Получает записи из таблицы.
+     *
+     * @param string $table Название таблицы.
+     * @param array $columns Массив с названиями колонок.
+     * @param array $conditions Ассоциативный массив условий (по умолчанию пустой).
+     * @return array|bool Возвращает массив записей или false в случае ошибки.
+     */
+    function read($table, $columns, $conditions=[])
     {
+        $columns = implode(', ', $columns);
         $sql = "SELECT {$columns} FROM {$table}";
 
         if (!empty($conditions)) {
             $whereClauses = [];
             foreach($conditions as $column => $value) {
-                $whereClauses = "{$column} =:{$value}";
+                $whereClauses[] = "{$column} = :{$column}";
             }
             $sql .=" WHERE " . implode(' AND ', $whereClauses);
         }
@@ -64,6 +76,15 @@ class Database {
             return false;
         }
     }
+
+    /**
+     * Обновляет записи в таблице.
+     *
+     * @param string $table Название таблицы.
+     * @param array $data Ассоциативный массив обновляемых данных (ключи — это названия колонок).
+     * @param array $conditions Ассоциативный массив условий для обновления.
+     * @return bool Возвращает true при успешном обновлении, иначе false.
+     */
     function update($table, $data, $conditions)
     {
         $setClauses = [];
@@ -73,7 +94,7 @@ class Database {
 
         $whereClauses = [];
         foreach($conditions as $column => $value) {
-            $whereClauses = "{$column} =:{$value}";
+            $whereClauses[] = "{$column} = :{$column}";
         }
 
         $sql = "UPDATE {$table} SET" .
@@ -91,11 +112,18 @@ class Database {
         }
     }
 
+    /**
+     * Удаляет записи из таблицы.
+     *
+     * @param string $table Название таблицы.
+     * @param array $conditions Ассоциативный массив условий для удаления.
+     * @return bool Возвращает true при успешном удалении, иначе false.
+     */
     function delete($table, $conditions)
     {
         $whereClauses = [];
         foreach($conditions as $column => $value) {
-            $whereClauses = "{$column} =:{$value}";
+            $whereClauses[] = "{$column} = :{$column}";
         }
 
         $sql = "DELETE FROM {$table} WHERE " . implode(' AND ', $whereClauses);
